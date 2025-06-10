c56fc646280cd7c1c605ce46433191c0ae17423c
Bruno Gomes <bgomes.89@gmail.com>
2025-06-10 14:26:45 -0300

    feat: menu ativo

--- src/components/Anchor.tsx
+++ src/components/Anchor.tsx
@@ -1,12 +1,17 @@
+import { useLocation } from "react-router-dom";
+
 type AnchorProps = {
     href: string;
     text: string;
 };
 
 export function Anchor({href, text}: AnchorProps){
+
+    const { pathname } = useLocation();
+    const isActive = pathname === href;
     return (
         <a href={href}>
-            <li className="px-6 py-6 hover:bg-[#F3F4F6] cursor-pointer text-[#4B5563] text-sm transition-all ">{text}</li>
+            <li className= {isActive ? "px-6 py-6 bg-[#F3F4F6] cursor-pointer text-[#4B5563] text-sm transition-all " : "px-6 py-6 hover:bg-[#F3F4F6] cursor-pointer text-[#4B5563] text-sm transition-all "}>{text}</li>
         </a>
         
     )
